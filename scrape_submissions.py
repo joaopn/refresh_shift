52b607c7b5182636a7606d718741e70eeb74d921
Joao PN <joaoxp@gmail.com>
2024-04-09 21:50:38 +0200

    Change scrape_submissions to load ids file partially

--- scrape_submissions.py
+++ scrape_submissions.py
@@ -6,20 +6,27 @@ import argparse
 from datetime import datetime
 from tqdm.asyncio import tqdm
 import os
+from itertools import islice
 
 def setup_logging(base_folder, outfile):
     # Set up logging
     log_file = os.path.join(base_folder, f"log/{os.path.splitext(os.path.basename(outfile))[0]}_scrapelog.log")
+    old_log_file = log_file + '.old'
+    if os.path.exists(old_log_file):
+        os.remove(old_log_file)
     if os.path.exists(log_file):
-        os.rename(log_file, log_file + '.old')
+        os.rename(log_file, old_log_file)
 
     logging.basicConfig(filename=log_file, level=logging.DEBUG,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # Set up error logging
     error_log_file = os.path.join(base_folder, f"log/{os.path.splitext(os.path.basename(outfile))[0]}_errorlog.log")
+    old_error_log_file = error_log_file + '.old'
+    if os.path.exists(old_error_log_file):
+        os.remove(old_error_log_file)
     if os.path.exists(error_log_file):
-        os.rename(error_log_file, error_log_file + '.old')
+        os.rename(error_log_file, old_error_log_file)
 
     error_logger = logging.getLogger('error_logger')
     error_logger.setLevel(logging.ERROR)
@@ -31,16 +38,13 @@ def setup_logging(base_folder, outfile):
 
     return error_logger
 
-async def scrape_submissions(base_folder, ids_file, auth_file, outfile, batch_size=100):
+
+async def scrape_submissions(base_folder, ids_file, auth_file, outfile, batch_size=1000, reddit_batch_size=100):
     error_logger = setup_logging(base_folder, outfile)
 
     # Read auth data from AUTH.json file
     with open(auth_file) as f:
         auth_data = json.load(f)
-
-    # Read submission IDs from file
-    with open(ids_file) as f:
-        submission_ids = [line.strip() for line in f]
 
     # Create the ids_processed folder if it doesn't exist
     ids_processed_folder = os.path.join(base_folder, os.path.dirname(ids_file), 'processed')
@@ -54,24 +58,6 @@ async def scrape_submissions(base_folder, ids_file, auth_file, outfile, batch_si
     else:
         processed_ids = set()
 
-    # Get the total number of submission IDs
-    submission_count_total = len(submission_ids)
-
-    # Remove already processed IDs from submission_ids
-    submission_ids = [id for id in submission_ids if id not in processed_ids]
-
-    # Add the 't3_' prefix to the remaining submission IDs
-    submission_ids = [f't3_{id}' for id in submission_ids]
-
-    # Get the total number of submission IDs after removing already processed IDs
-    submission_count = len(submission_ids)
-
-    # Print the total number of submission IDs and the number of IDs already processed
-    print(f"Total submission IDs: {submission_count}. Skipping {submission_count_total - submission_count} already processed IDs.")
-
-    # Remove already processed IDs from submission_ids
-    submission_ids = [id for id in submission_ids if id not in processed_ids]
-
     # Initialize asyncPRAW Reddit instance
     reddit = asyncpraw.Reddit(
         client_id=auth_data['client_id'],
@@ -83,110 +69,129 @@ async def scrape_submissions(base_folder, ids_file, auth_file, outfile, batch_si
 
     # Open the output file for writing
     with open(outfile, 'a') as f:
-        with tqdm(total=len(submission_ids), desc=f"{os.path.basename(ids_file)}") as pbar:
-            for i in range(0, len(submission_ids), batch_size):
-                batch_ids = submission_ids[i:i+batch_size]
-                processed_batch_ids = []
-                try:
-                    # Fetch submissions in batches using reddit.info()
-                    async for submission in reddit.info(fullnames=batch_ids):
-                        
-                        # Get all fields that are already loaded using vars()
-                        submission_data = vars(submission)
+        with open(ids_file) as ids_f:
+            total_ids = sum(1 for _ in ids_f)
+            ids_f.seek(0)  # Reset the file pointer to the beginning
 
-                        # Handle non-serializable objects
-                        if '_reddit' in submission_data:
-                            submission_data.pop('_reddit')
-                        if 'comments' in submission_data:
-                            submission_data.pop('comments')
-                        if 'selftext_html' in submission_data:
-                            submission_data.pop('selftext_html')
-                        if 'subreddit' in submission_data:
-                            submission_data['subreddit'] = submission_data['subreddit'].display_name
-                        if 'author' in submission_data:
-                            submission_data['author'] = submission_data['author'].name if submission_data['author'] else None
-                        if 'poll_data' in submission_data:
-                            poll_data = submission_data['poll_data']
-                            if poll_data:
-                                submission_data['poll_data'] = {
-                                    'options': [
-                                        {
-                                            'text': option.text,
-                                            'vote_count': option.vote_count,
-                                            'id': option.id
+            with tqdm(total=total_ids, desc=f"{os.path.basename(ids_file)}") as pbar:
+                while True:
+                    batch_ids = [line.strip() for line in islice(ids_f, batch_size)]
+                    if not batch_ids:
+                        break
+
+                    # Remove already processed IDs from batch_ids
+                    batch_ids = [id for id in batch_ids if id not in processed_ids]
+
+                    # Add the 't3_' prefix to the remaining submission IDs
+                    batch_ids = [f't3_{id}' for id in batch_ids]
+
+                    processed_batch_ids = []
+                    try:
+                        # Fetch submissions in smaller batches using reddit.info()
+                        for i in range(0, len(batch_ids), reddit_batch_size):
+                            reddit_batch = batch_ids[i:i+reddit_batch_size]
+                            async for submission in reddit.info(fullnames=reddit_batch):
+                            
+                                # Get all fields that are already loaded using vars()
+                                submission_data = vars(submission)
+
+                                # Handle non-serializable objects
+                                if '_reddit' in submission_data:
+                                    submission_data.pop('_reddit')
+                                if 'comments' in submission_data:
+                                    submission_data.pop('comments')
+                                if 'selftext_html' in submission_data:
+                                    submission_data.pop('selftext_html')
+                                if 'subreddit' in submission_data:
+                                    submission_data['subreddit'] = submission_data['subreddit'].display_name
+                                if 'author' in submission_data:
+                                    submission_data['author'] = submission_data['author'].name if submission_data['author'] else None
+                                if 'poll_data' in submission_data:
+                                    poll_data = submission_data['poll_data']
+                                    if poll_data:
+                                        submission_data['poll_data'] = {
+                                            'options': [
+                                                {
+                                                    'text': option.text,
+                                                    'vote_count': option.vote_count,
+                                                    'id': option.id
+                                                }
+                                                for option in poll_data.options
+                                            ] if hasattr(poll_data, 'options') else [],
+                                            'total_vote_count': poll_data.total_vote_count if hasattr(poll_data, 'total_vote_count') else None,
+                                            'user_selection': poll_data.user_selection.text if hasattr(poll_data, 'user_selection') and poll_data.user_selection else None,
+                                            'voting_end_timestamp': poll_data.voting_end_timestamp if hasattr(poll_data, 'voting_end_timestamp') else None
                                         }
-                                        for option in poll_data.options
-                                    ] if hasattr(poll_data, 'options') else [],
-                                    'total_vote_count': poll_data.total_vote_count if hasattr(poll_data, 'total_vote_count') else None,
-                                    'user_selection': poll_data.user_selection.text if hasattr(poll_data, 'user_selection') and poll_data.user_selection else None,
-                                    'voting_end_timestamp': poll_data.voting_end_timestamp if hasattr(poll_data, 'voting_end_timestamp') else None
-                                }
-                            else:
-                                submission_data['poll_data'] = None
+                                    else:
+                                        submission_data['poll_data'] = None
 
 
-                        # Check for non-serializable objects
-                        non_serializable_keys = []
-                        for key, value in submission_data.items():
-                            try:
-                                json.dumps(value)
-                            except TypeError:
-                                non_serializable_keys.append(key)
+                                # Check for non-serializable objects
+                                non_serializable_keys = []
+                                for key, value in submission_data.items():
+                                    try:
+                                        json.dumps(value)
+                                    except TypeError:
+                                        non_serializable_keys.append(key)
 
-                        # Remove non-serializable objects from the submission data
-                        for key in non_serializable_keys:
-                            submission_data.pop(key)
+                                # Remove non-serializable objects from the submission data
+                                for key in non_serializable_keys:
+                                    submission_data.pop(key)
 
-                        if non_serializable_keys:
-                            error_logger.error(f"Non-serializable objects found in submission {submission.id}: {', '.join(non_serializable_keys)}")
-                            continue  # Skip writing to file and adding to processed IDs
+                                if non_serializable_keys:
+                                    error_logger.error(f"Non-serializable objects found in submission {submission.id}: {', '.join(non_serializable_keys)}")
+                                    continue  # Skip writing to file and adding to processed IDs
 
-                        # Add retrieved_utc_new field with the current timestamp
-                        submission_data['retrieved_utc'] = int(datetime.now().timestamp())
+                                # Add retrieved_utc_new field with the current timestamp
+                                submission_data['retrieved_utc'] = int(datetime.now().timestamp())
 
-                        # Write the submission data to the output file
-                        f.write(json.dumps(submission_data) + '\n')
+                                # Write the submission data to the output file
+                                f.write(json.dumps(submission_data) + '\n')
 
-                        # Add the successfully processed ID to the list
-                        processed_batch_ids.append(submission.id)
+                                # Add the successfully processed ID to the list
+                                processed_batch_ids.append(submission.id)
 
-                    # Flush data to disk after each batch
-                    f.flush()
+                        # Flush data to disk after each batch
+                        f.flush()
 
-                    # Write processed IDs to file only if there were no errors
-                    with open(processed_ids_file, 'a') as processed_f:
-                        for id in processed_batch_ids:
-                            processed_f.write(f"{id}\n")
+                        # Write processed IDs to file only if there were no errors
+                        with open(processed_ids_file, 'a') as processed_f:
+                            for id in processed_batch_ids:
+                                processed_f.write(f"{id}\n")
 
-                    # Update the progress bar with the batch size
-                    pbar.update(len(batch_ids))
+                        # Update the progress bar with the batch size
+                        pbar.update(len(batch_ids))
 
-                except Exception as e:
-                    error_message = str(e)
-                    if "Cannot connect to host" in error_message or "Connect call failed" in error_message:
-                        # Skip adding IDs to processed file if there was an internet outage error
-                        error_logger.error(f"Connection error scraping batch starting from {batch_ids[0]}: {error_message}")
-                    else:
-                        # Log the error with the specific submission ID
-                        submission_id = submission.id if 'submission' in locals() else 'Unknown'
-                        error_logger.error(f"Error scraping submission {submission_id}: {error_message}")
+                    except Exception as e:
+                        error_message = str(e)
+                        if "Cannot connect to host" in error_message or "Connect call failed" in error_message:
+                            # Skip adding IDs to processed file if there was an internet outage error
+                            error_logger.error(f"Connection error scraping batch starting from {batch_ids[0]}: {error_message}")
+                        else:
+                            # Log the error with the specific submission ID
+                            submission_id = submission.id if 'submission' in locals() else 'Unknown'
+                            error_logger.error(f"Error scraping submission {submission_id}: {error_message}")
+
     await reddit.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scrape Reddit submissions using asyncPRAW')
     parser.add_argument('--datasets', type=str, required=True, help='Datasets to scrape submissions for')
     parser.add_argument('--auth', type=str, default='auth/AUTH.json', help='File containing Reddit API authentication data')
+    parser.add_argument('--batch_mb', type=int, default=100, help='Batch size in MB for writing data to disk')
     parser.add_argument('--datatype', type=str, choices=['submissions', 'comments'], default='submissions', help='Type of data to scrape')
     parser.add_argument('--basefolder', type=str, default='', help='Base folder for data writing')
     args = parser.parse_args()
 
     datasets = args.datasets.split(',')
+    batch_size = int(args.batch_mb * 1024 * 1024/(7*4))
+    print(f"Batch size: {batch_size}")
 
     for dataset in datasets:
         if args.datatype == 'submissions':
             ids_file = os.path.join(args.basefolder, f'data/ids/submission_ids_{dataset}.csv')
             outfile = os.path.join(args.basefolder, f'data/submissions_{dataset}.ndjson')
-            asyncio.run(scrape_submissions(args.basefolder, ids_file, args.auth, outfile))
+            asyncio.run(scrape_submissions(args.basefolder, ids_file, args.auth, outfile, batch_size=batch_size))
 
         elif args.datatype == 'comments':
             raise NotImplementedError("Comment scraping not implemented yet")
